a2d10deffb941731d2aacdc3109829214f98d9da
preethamreddy` <preethamreddy03111@gmail.com>
2026-05-12 14:16:27 +0530

    fix unnamed column issue

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,6 +5,6 @@ import streamlit as st
 def load_data():
     file_id = "1jeS0eD-g2QQIROaUApSm_NXXvbO4WQSv"
     url = f"https://drive.usercontent.google.com/download?id={file_id}&confirm=t"
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, low_memory=False, index_col=0)
     df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
     return df
